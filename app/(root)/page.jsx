d9d96ddd46b3ecfba0177044cc96745b86113716
Farhan Yousafzai <coderwithferry@gmail.com>
2025-08-13 20:12:19 +0500

    Frotend Project

---
+++ app/(root)/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <div className='flex items-center justify-center'>
+        <h1 className='text-5xl text-green-300'>Agency.ai</h1>
+      </div>
+    </>
+  )
+}
+
+export default page
